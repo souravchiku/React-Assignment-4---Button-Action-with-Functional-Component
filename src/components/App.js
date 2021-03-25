3849ee885644dfa2f46111801a57c6e8d640682f
sourav <souravchiku50@gmail.com>
2021-03-26 01:09:02 +0530

    function-button

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
+
 function App() {
+
+  const[data,setData]=useState("")
+  function fun(){
+    setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main  */}
+      <p id="para">{data}</p>
+      <button onClick={()=>fun()}>Update</button>
     </div>
   );
 }

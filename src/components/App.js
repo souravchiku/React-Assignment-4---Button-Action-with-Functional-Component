2cf430ba13137190456b80ca0d708535f387f3a0
sourav <souravchiku50@gmail.com>
2021-04-03 06:04:23 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,46 @@
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
+
+//import "./../styles/App.css";
 
 
+ 
 
 function App() {
 
-  const[data,setData]=useState(false)
-  function fun(){
-    setData(true)
-  }
-  return (
-    <div id="main">
-      {/* // Do not alter the main  */}
-      {data && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
-      <button id="click"onClick={fun}>Update</button>
-    </div>
-  );
+const [flag, useFlag] = useState(false);
+
+const fun = () => {
+
+useFlag(true);
+
+
+ 
+
+};
+
+
+
+ 
+
+return (
+
+<div id="main">
+
+<button id="click" onClick={fun}>Button</button>
+
+{ flag && <p id="para">
+
+Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy
+
+</p>}
+
+</div>
+
+);
+
 }
 
+
+ 
 
 export default App;
